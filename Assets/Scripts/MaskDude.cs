fd15ab81dd18ace062f9972bd50bea44e72a3553
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2022-05-17 18:21:33 -0400

    adicionar inimigo mask dude

--- Assets/Scripts/MaskDude.cs
+++ Assets/Scripts/MaskDude.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MaskDude : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public float speed;
+
+    public Transform rightCol;
+    public Transform leftCol;
+
+    public Transform headPoint;
+
+    private bool colliding;
+
+    public LayerMask layer;
+
     void Start()
     {
-        
+        TryGetComponent(out rb);
+        TryGetComponent(out anim);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x*-1f, transform.localScale.y);
+            speed *= -1f;
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            float height = col.contacts[0].point.y - headPoint.position.y;
+
+            if (height > 0f)
+            {
+                col.gameObject.GetComponent<Player>().ImpulseForce(5f);
+                anim.SetTrigger("die");
+                speed = 0f;
+                rb.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.35f);
+            }
+        }
     }
 }
