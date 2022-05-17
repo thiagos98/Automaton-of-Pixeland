91b6a44f1889b744b3ec603d789036cd21195e7f
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2022-05-16 23:59:14 -0400

    adicionar coletáveis com feedback visual

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+
+    private GameObject collected;
+    void Start()
+    {
+        TryGetComponent(out boxCollider);
+        TryGetComponent(out spriteRenderer);
+        collected = transform.GetChild(0).gameObject;
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            spriteRenderer.enabled = false;
+            boxCollider.enabled = false;
+            collected.SetActive(true);
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
