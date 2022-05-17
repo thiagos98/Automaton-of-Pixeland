7cc75d077db237a89ad9ad82e49faf956ca2c998
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2022-05-17 16:01:04 -0400

    adicionar mecanica da serra e adicionar level 3

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    public float speed;
+    public float moveTime;
+    
+    private bool dirRight = true;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dirRight)
+        {
+            // serra vai para direita
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            // serra vai para esquerda
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        timer += Time.deltaTime;
+        
+        if(timer > moveTime)
+        {
+            dirRight = !dirRight;
+            timer = 0f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<Player>().Die();
+        }
+    }
+}
