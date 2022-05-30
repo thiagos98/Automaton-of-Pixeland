816f4783a6b28d4b4530c8034b1ca5286e8bab31
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2022-05-30 18:38:38 -0400

    refatoração do projeto

---
+++ Assets/Scripts/GenerateObjects/GenerateCollectables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateCollectables : MonoBehaviour
+{
+    public GameObject collectable;
+    
+    public Vector3 position;
+    public Tilemap tilemap;
+    private float tile_anchor;
+
+    private void Start()
+    {
+        tile_anchor = tilemap.tileAnchor.x;
+        Generate();
+    }
+
+    private void Generate()
+    {
+        Instantiate(collectable, position, Quaternion.identity);
+        
+    }
+    
+}    
+
