2ef38e118a9b8a710b41ac3c29bc7e3eb6e332ce
Thiago Borges <tsb.snf16@uea.edu.br>
2022-08-20 19:38:49 -0400

    feat(Generator) Starting the cellular automata algorithm, starting the cell grid

--- Assets/Scripts/TilesGenerators/Cell.cs
+++ Assets/Scripts/TilesGenerators/Cell.cs
@@ -1,34 +1,72 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
-public class Cell : MonoBehaviour
+namespace TilesGenerators
 {
-    public Tilemap tilemap;
-    public RuleTile tile;
-    
-    public int minWidth = -16;
-    public int maxWidth = 14;
-    
-    public int minHeight = -8;
-    public int maxHeight = -1;
-
-    private void Update()
+    public class Cell : MonoBehaviour
     {
-        for (var i = minWidth; i < maxWidth; i++)
+        public Tilemap tilemap;
+        public RuleTile tile;
+
+        public const int width = 31;
+        public const int height = 7;
+    
+        public int minWidth = -16;
+        public int maxWidth = 14;
+    
+        public int minHeight = -8;
+        public int maxHeight = -1;
+        
+        public bool[,] cellmap = new bool[width, height];
+        public float chanceToStartAlive = 0.75f;
+
+        private void Start()
         {
-            for (var j = minHeight; j < maxHeight; j++)
+            InitialiseMap(cellmap);
+            
+        }
+
+        private void Update()
+        {
+            ShowMap();
+        }
+
+        public void ShowMap()
+        {
+            for (int x = 0; x < width; x++)
             {
-                tilemap.SetTile(new Vector3Int(i, j, 0), tile);
+                for (int y = 0; y < height; y++)
+                {
+                    if (cellmap[x, y])
+                    {
+                        tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                    }
+                }
             }
         }
-        
+
+        public bool[,] InitialiseMap(bool[,] map)
+        {
+            tilemap.transform.position = gameObject.transform.localPosition;
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height-1; j++)
+                {
+                    if (Random.Range(0f, 1f) < chanceToStartAlive)
+                    {
+                        map[i, j] = true;
+                    }
+                }
+            }
+
+            return map;
+        }
     }
-}
 
-public class CellularAutomata : MonoBehaviour
-{
-
+    public class CellularAutomata : MonoBehaviour
+    {
+        // tilemap.SetTile(new Vector3Int(i, j, 0), tile);
+    }
 }
